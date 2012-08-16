b0b99c6d365f5a77d5aec664ac7d5f5b0e892f53
Jimmy Hedman <jimmy.hedman@southpole.se>
2012-08-16 15:35:37 +0200

    Corrected url.

--- crapnet-unittest.py
+++ crapnet-unittest.py
@@ -5,7 +5,8 @@ import unittest
 from xmlrpclib import ServerProxy, Error
 
 # server = ServerProxy("http://localhost:8000") # local server
-server = ServerProxy("http://192.168.128.1/cgi-bin/crapnet/do2.lua")
+#server = ServerProxy("http://192.168.128.1/cgi-bin/crapnet/do2.lua")
+server = ServerProxy("http://192.168.128.1/cgi-bin/crapnet/do.lua")
 #server = ServerProxy("http://192.168.128.1/cgi-bin/crapnet/cgitest.lua")
 
 print server
