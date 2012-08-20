91d91c3a752c0a7d407cd4228e31fab46cddbc70
Jimmy Hedman <jimmy.hedman@southpole.se>
2012-08-20 21:45:48 +0200

    Changed url to wrapper.

--- crapnet-unittest.py
+++ crapnet-unittest.py
@@ -5,11 +5,10 @@ import unittest
 from xmlrpclib import ServerProxy, Error
 
 # server = ServerProxy("http://localhost:8000") # local server
-#server = ServerProxy("http://192.168.128.1/cgi-bin/crapnet/do2.lua")
-server = ServerProxy("http://192.168.128.1/cgi-bin/crapnet/do.lua")
+server = ServerProxy("http://192.168.128.1/cgi-bin/crapnet/wrapper.lua")
+#server = ServerProxy("http://192.168.128.1/cgi-bin/crapnet/do.lua")
 #server = ServerProxy("http://192.168.128.1/cgi-bin/crapnet/cgitest.lua")
 
-print server
 print server.hello()
 
 
